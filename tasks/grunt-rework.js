fa4af1aa8fa440cc2ea071e54b68ba07427165e7
Donald Pipowitch <pipo@senaeh.de>
2014-08-14 16:29:32 +0200

    write source map into its own file
    If `sourcemapAsObject` is true write source map into its own file. You can even optionally  set `sourceMapFilename`, `sourceMapURL` and `sourceMapRootpath` like in `grunt-contrib-less`.

--- tasks/grunt-rework.js
+++ tasks/grunt-rework.js
@@ -42,9 +42,37 @@ module.exports = function(grunt) {
 
         var dest = _.isFunction(options.processName) ? options.processName(srcFile, res) : file.dest;
 
-        grunt.file.write(dest, res);
+        // if sourcemapAsObject is true, write source map into its own file
+        if(options.toString && options.toString.sourcemapAsObject) {
+          // if sourceMapFilename isn't set use dest with '.map' suffix
+          var mapDest = options.toString.sourceMapFilename || dest + '.map';
 
-        grunt.log.writeln('File "' + dest + '" created.');
+          // if sourceMapURL isn't set use sourceMapFilename
+          var sourceMapURL = options.toString.sourceMapURL || options.toString.sourceMapFilename;
+          res.code = res.code + '\n/*# sourceMappingURL=' + sourceMapURL + ' */';
+
+          // if sourceMapRootpath is set, use it as prefix for res.map.sources
+          if(options.toString.sourceMapRootpath) {
+            var sourceMapRootpath = options.toString.sourceMapRootpath;
+            // append '/' if necessary
+            if(sourceMapRootpath.charAt(sourceMapRootpath.length - 1) !== '/') {
+              sourceMapRootpath += '/';
+            }
+            res.map.sources.forEach(function(source, i) {
+              res.map.sources[i] = sourceMapRootpath + source;
+            });
+          }
+
+          grunt.file.write(dest, res.code);
+          grunt.file.write(mapDest, JSON.stringify(res.map));
+
+          grunt.log.writeln('File "' + dest + '" created.');
+          grunt.log.writeln('File "' + mapDest + '" created.');
+        } else {
+          grunt.file.write(dest, res);
+
+          grunt.log.writeln('File "' + dest + '" created.');
+        }
 
         nextFile();
       }, next);
